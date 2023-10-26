fc54022c27dfc042b0c925e5cbd57c8fcb00f254
Ron Dagdag <ron@dagdag.net>
2023-10-25 23:16:20 -0500

    add say

--- 11_planexecute.js
+++ 11_planexecute.js
@@ -10,35 +10,55 @@ import { DynamicTool } from "langchain/tools";
 import { OpenAIEmbeddings } from "langchain/embeddings/openai";
 import { FaissStore } from "langchain/vectorstores/faiss";
 import { VectorDBQAChain } from "langchain/chains";
+import { exec } from 'child_process';
 
 function getTodayDateTime() {
-    const timeZone = 'America/Chicago';
-    const options = {
-        timeZone: timeZone,
-        year: 'numeric',
-        month: 'long',
-        day: 'numeric',
-        weekday: 'long',
-        hour: '2-digit',
-        minute: '2-digit',
-        second: '2-digit',
-        hour12: true
-    };
-    const today = new Date();
-    const formattedDate = today.toLocaleString('en-US', options);
-    const result = {
-        "formattedDate": formattedDate,
-        "timezone": timeZone
-    };
-    return JSON.stringify(result);
-  }
+  const timeZone = 'America/Chicago';
+  const options = {
+      timeZone: timeZone,
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+      weekday: 'long',
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
+      hour12: true
+  };
+  const today = new Date();
+  const formattedDate = today.toLocaleString('en-US', options);
+  const result = {
+      "formattedDate": formattedDate,
+      "timezone": timeZone
+  };
+  return JSON.stringify(result);
+}
   
-  const dateTool = new DynamicTool({
-    name: "todays_date_time",
-    description:
-      "Useful to get current day, date and time.",
-    func: async () => getTodayDateTime(),
+const dateTool = new DynamicTool({
+  name: "todays_date_time",
+  description:
+    "Useful to get current day, date and time.",
+  func: async () => getTodayDateTime(),
+});
+
+  
+const saySomething = (text) => {
+  console.log(`Saying: ${text}`);
+  exec(`say ${text}`, (error) => {
+    if (error) {
+      console.error(`Error: ${error}`);
+    }
   });
+  return "I said it.";
+};
+
+const sayTool = new DynamicTool({
+  name: "harrybot_say",
+  description:
+    "call this to let the bot to say something, passing the text as input.",
+  func: async (text) => saySomething(text),
+  returnDirect: true,
+});
 
 const model = new ChatOpenAI({
     temperature: 0,
@@ -54,12 +74,12 @@ const chain = VectorDBQAChain.fromLLM(model, vectorStore);
 const qaTool = new ChainTool({
     name: "Harry_Potter_Sorcerers_Stone_QA",
     description:
-      "Harry Potter QA - useful for when you need to ask questions about Harry Potter",
+      "Useful for when you need to ask questions about Harry Potter books",
     chain: chain,
     returnDirect: true,
   });
 
-const tools = [new Calculator(), dateTool, qaTool];
+const tools = [new Calculator(), dateTool, qaTool, sayTool];
 
 const executor = PlanAndExecuteAgentExecutor.fromLLMAndTools({
   llm: model,
@@ -67,7 +87,13 @@ const executor = PlanAndExecuteAgentExecutor.fromLLMAndTools({
 });
 
 const result = await executor.call({
-  input: `How many houses in Harry Potter? Multiply the result by today's year.`,
+  input: `How many Hogwart houses in Harry Potter? Multiply the result by today's year. Tell the bot to say the answer`,//
 });
+
+//await saySomething("Hello, I am the Harry Potter robot. Ask me a question.");
+
+// const result = await executor.call({
+//   input: `Tell the bot tool to say "who are you?"`,//
+// });
 
 console.log(result);
